9a8cfb042d85939869231cd82e6aaf2096d77804
smirok <smirok123@mail.ru>
2021-05-31 00:12:35 +0300

    Fixed ImageService methods

--- src/main/java/server/core/service/ImageService.java
+++ src/main/java/server/core/service/ImageService.java
@@ -11,23 +11,30 @@ import java.util.Base64;
 
 @Service
 public class ImageService {
-
     public static final String imagesDirectory = "./images/";
 
     public static final String ERROR = "ERROR";
 
     public static String saveTourImage(TourDTO tour) {
+        if (tour.getImage() == null) {
+            return null;
+        }
         byte[] image = Base64.getDecoder().decode(tour.getImage());
         String imagePath = imagesDirectory +  tour.getGuide() + "/" + tour.getId();
         try {
+            Files.createDirectories(Path.of(imagesDirectory +  tour.getGuide()));
             Files.write(Path.of(imagePath), image);
         } catch (IOException ex) {
+            ex.printStackTrace();
             return ERROR;
         }
         return imagePath;
     }
 
     public static String getTourImageCode(Tour tour) {
+        if (tour.getImage() == null) {
+            return null;
+        }
         try {
             byte[] image = Files.readAllBytes(Path.of(tour.getImage()));
             return Base64.getEncoder().encodeToString(image);
